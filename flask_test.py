fd9e5aa9072dd2f753156ccbd5a11fc6492586c1
alvin C <alvinchristianto93@gmail.com>
2022-04-07 16:36:57 +0700

    adding unit test and variable for flask shell commommand

--- flask_test.py
+++ flask_test.py
@@ -3,4 +3,13 @@ from app.models import User, Post
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
+    return {
+        'db': db, 
+        'User': User, 
+        'Post': Post,
+        'getAllUser': User.query.all(),
+        'getAllPost': Post.query.all(),
+        'john': User.query.get(1),
+        'alvin': User.query.get(2),
+        'susan': User.query.get(3)
+    }
