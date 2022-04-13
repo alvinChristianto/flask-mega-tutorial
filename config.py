985d27ffe29b5d1c1508d3278691a66aa10794a4
alvin C <alvinchristianto93@gmail.com>
2022-04-13 12:49:08 +0700

    mega-cp9-10 epagiagiantion post, email support changing password

--- config.py
+++ config.py
@@ -11,4 +11,6 @@ class Config(object):
   MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
   MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
   MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-  ADMINS = ['tiusdsaklawase@gmail.com']
+  ADMINS = ['alvinchristianto93@gmail.com']
+
+  POSTS_PER_PAGE = 3
